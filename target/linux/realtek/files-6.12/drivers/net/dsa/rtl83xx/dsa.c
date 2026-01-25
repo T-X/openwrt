6054ab5f05188a03d115514ce6d33cf225a34f7c
Linus Lüssing <linus.luessing@c0d3.blue>
2026-01-25 12:26:15 +0100

    fixup: setup special portmasks only once

--- target/linux/realtek/files-6.12/drivers/net/dsa/rtl83xx/dsa.c
+++ target/linux/realtek/files-6.12/drivers/net/dsa/rtl83xx/dsa.c
@@ -448,24 +448,14 @@ static void rtldsa_vlan_set_pvid(struct rtl838x_switch_priv *priv,
 	priv->ports[port].pvid = pvid;
 }
 
-static void rtldsa_83xx_mc_pmasks_setup(struct rtl838x_switch_priv *priv)
+void rtldsa_83xx_mc_pmasks_setup(struct rtl838x_switch_priv *priv)
 {
-	u64 portmask = 0;
-
 	/* RTL8380 and RTL8390 use an index into the portmask table to set the
 	 * unknown multicast portmask, setup a default at a safe location
 	 * On RTL93XX, the portmask is directly set in the profile,
 	 * see e.g. rtl9300_vlan_profile_setup
 	 */
-	if (priv->family_id == RTL8380_FAMILY_ID)
-		portmask = RTL838X_MC_PMASK_ALL_PORTS;
-	else if (priv->family_id == RTL8390_FAMILY_ID)
-		portmask = RTL839X_MC_PMASK_ALL_PORTS;
-	else
-		dev_err(priv->dev, "%s: unknown family_id %u\n", __func__,
-			priv->family_id);
-
-	priv->r->write_mcast_pmask(MC_PMASK_ALL_PORTS_IDX, portmask);
+	priv->r->write_mcast_pmask(MC_PMASK_ALL_PORTS_IDX, ~0);
 	priv->r->write_mcast_pmask(MC_PMASK_MIN_PORTS_IDX, priv->mc_router_portmask);
 }
 
